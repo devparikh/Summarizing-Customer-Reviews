e76ae5d638fb4f42c060247e0329d9e57230244e
Dev Parikh <47342287+devparikh@users.noreply.github.com>
2021-10-03 12:39:38 -0400

    Update preprocessingdata.py

--- preprocessingdata.py
+++ preprocessingdata.py
@@ -4,10 +4,13 @@ import tensorflow as tf
 import numpy as np
 import nltk
 from nltk.corpus import stopwords, wordnet
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.preprocessing.text import Tokenizer
+import re
+import contractions
+from autocorrect import Speller
+from nltk.stem import SnowballStemmer
 
-nltk.download("all")
+#nltk.download("all")
 
 # Loading in the data
 review_data = pd.read_csv("C:\\Users\\me\\Documents\\Text-Summarization\\Reviews.csv", index_col=0)
@@ -49,55 +52,52 @@ cleaned_text = []
 
 stop_words = set(stopwords.words('english'))
 
-
 summary = review_dataset["Summary"]
 text = review_dataset["Text"]
 
+cleaned_text = []
+cleaned_summary = []
 
 # A function that will do all of the preprocessing that we need to do for our data
 def preprocessing_text(dataset, empty_dataset):
     for sentence in dataset:
-        tokenized_sentence = word_tokenize(sentence)
+        # We are tokenizing the text
+        # when using this tensorflow function we are automatically removing punctuation and other randon characters
+        tokenizer = Tokenizer(num_words = 100, oov_token="UNK")
+        tokenized_sentence = tokenizer.fit_on_texts(sentence)
 
-        clean_sentence = []
+        # We are removing all non-alphanumeric values in sentences
         for word in tokenized_sentence:
             # removing non-alphanumeric elements from our text
             if word.isalnum() == False and word != ' ':
                 tokenized_sentence.remove(word)
 
+        # Here we are removing all stop words
         for word in tokenized_sentence:
             # removing stop words from our text
             if word.lower() in stop_words:
                 tokenized_sentence.remove(word)
-
-        clean_sentence = []
-        reviews_sentence = []
-        new_sentence = []
-
-        for sentence in input_data:
-            for word in sentence:
-                for character in word:
-                    if character not in punctuation:
-                        word = re.sub(r'[^\w\s]', '', word)
-                        new_sentence.append(word)
-
-            clean_sentence = ''.join(new_sentence)
         
-        for word in clean_sentence:
+        clean_sentence = []
+        for word in tokenized_sentence:
             # performing stemming
-            
             snowball_stemmer = SnowballStemmer('english')    
             # Using the Snowball Stemmer algorithm on each of the words of the updated list
             word = snowball_stemmer.stem(word)
-            reviews_sentence.append(word)
+            clean_sentence.append(word)
             sentence = ''.join(clean_sentence)
 
         # lowercasing the text
         sentence = sentence.lower()
 
+        # here we are essentially converting all of the text to the cooresponding numeric value for the model to understand
+        indexed_sentence = tokenizer.word_index(sentence)
+        # here we are taking these numeric values and then putting them into a sequence
+        sequenced_sentence = tokenizer.texts_to_sequences(indexed_sentence)
+
         # adding all of the updated values to the final list
-        empty_dataset.append(sentence)
- 
+        empty_dataset.append(sequenced_sentence)
+
 preprocessing_text(review_text, cleaned_text)
 preprocessing_text(review_summary, cleaned_summary)
 
